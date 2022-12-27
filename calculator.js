2b945dc8d4cf7c0fa051b69430e1b19cde0873a8
Momo <mobuttondev@gmail.com>
2022-12-26 21:59:03 -0500

    calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/index.html");
+})
+
+
+app.post("/", function(req, res){
+	let num1 = Number(req.body.num1);
+	let num2 = Number(req.body.num2);
+	let result = num1 + num2;
+
+	res.send(`The result of the calculation is: ${result}`);
+	//console.log(req.body);
+	//console.log(req.body.num1);
+})
+
+
+app.listen(3000, function(){
+	console.log("port started 3000");
+})
